c50022fd294d0b162245df4e41a414b4cbb41367
akrbva <a.krbva@gmail.com>
2022-07-04 12:32:37 +0600

    oop shop models

--- oop_shop/main.py
+++ oop_shop/main.py
@@ -1,8 +1,10 @@
-from models import User
-user1 = User('test@gmail.com', 'hello', 'female')
-user1.register('123456789', '123456789')
-user1.login('123456789')
-print(user1.is_authenticated)
+from models import User, Product, Comment
 
-product1 = Product('Iphone10', 1234567, '...', 18)
-comment1 = Comment(user1, product1, 'Very Good!')
+user1 = User("test@gmail.com", "hello", "female")
+user1.register("123456789", "123456789")
+user1.login("123456789")
+
+product1 = Product("Iphone 10", 12345, "...", 10)
+
+comment1 = Comment(user1, product1, "Оч классный телефон")
+print(comment1)
--- oop_shop/models.py
+++ oop_shop/models.py
@@ -1,8 +1,11 @@
+import permissions
+
 class Category:
     def __init__(self, title):
         self.title = title
+    
     def __str__(self):
-
+        return self.title
 
 class Product:
     def __init__(self, title, price, description, quantity):
@@ -10,16 +13,21 @@ class Product:
         self.price = price
         self.desc = description
         self.quantity = quantity
-    
+
+    def __str__(self):
+        return f"{self.title} [{self.quantity}] - ${self.price}\n({self.desc[:20]})"
+
 class Comment:
     def __init__(self, user, product, body):
         permissions.login_required(user)
         from datetime import datetime
-        
         self.user = user
         self.product = product
         self.body = body
-
+        self.created_at = datetime.now()
+    
+    def __str__(self):
+        return f"{self.user.email} - [{self.created_at}] - {self.body}"
 
 class User:
     def __init__(self, email, name, sex):
@@ -28,21 +36,24 @@ class User:
         self.sex = sex
         self.__password = None
         self.is_authenticated = False
-        print(f'Успешно создан юзер {self.email}')
+        print(f"успешно создан юзер {self.email}")
 
     def register(self, password, password_confirm):
         if password != password_confirm:
             raise Exception("Пароли не совпадают")
         self.__password = password
-        print(f'Успешно зарегестрирован юзер {self.email}')
+        print(f"успешно зарегистрирован юзер {self.email}")
 
     def login(self, password):
         if self.__password != password:
             raise Exception("Пароль не верный")
         self.is_authenticated = True
+        print(f"успешно залогинился юзер {self.email}")
 
     def logout(self):
-        permissions.login_require(self)
+        permissions.login_required(self)
         self.is_authenticated = False
-        print(f'Успешно вошел юзер {self.email}')
-    def 
+        print(f"успешно вышел юзер {self.email}")
+
+    def __str__(self):
+        return self.email
